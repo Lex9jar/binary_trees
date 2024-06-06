064ef3d8b9541798bc34099c265bcff8a37c2952
Lex9jar <alexanderedim89@gmail.com>
2024-06-06 15:24:38 +0100

    Checks if a binary tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -47,7 +47,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_full - Checks if a binary tree is a perfect binary tree.
+ * binary_tree_is_full - Checks if a binary tree is full.
  * @tree: Pointer to a tree to compute.
  *
  * Return: 0 if tree is NULL or not full, otherwise 1 if full.
